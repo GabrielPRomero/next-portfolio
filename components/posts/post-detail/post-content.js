b51af79a8b44e60d9e400aad6290e8279436f872
gabriel <80391539+GabrielPRomero@users.noreply.github.com>
2021-12-25 14:05:41 +0200

    optimized syntaxhighlighter package

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -3,8 +3,11 @@ import classes from "./post-content.module.css";
 import PostHeader from "./post-header";
 import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 import Image from "next/image";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
+import atomDark from "react-syntax-highlighter/dist/cjs/styles/prism/atom-dark";
+import js from "react-syntax-highlighter/dist/cjs/languages/prism/javascript";
+
+SyntaxHighlighter.registerLanguage("js", js);
 
 export default function PostContent({ post }) {
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
